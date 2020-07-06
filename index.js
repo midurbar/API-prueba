48826d7ab0646fe4ea250509bf6a5ee28cc31321
afd <afd@alup5.academiapostal.es.local>
2020-07-06 20:53:26 +0200

    mas cambios en el index

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const {Poblacion} = require('./models')
 
 
 app.get("/", function (req, res) {
     res.json("hola");
 })
 
+app.get("/poblaciones", function (req, res) {
+    Poblacion.findall()
+    .then(poblaciones => res.json(poblaciones))
+    .catch(err => res.json(err))
+})
 
 app.listen(3000)
